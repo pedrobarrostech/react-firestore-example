591b3d4d9e910bd61a2226c837b736a01631921d
Pedro Barros <pedro.barros@entelgy.com>
2018-09-05 09:54:06 -0300

    improvements

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -22,7 +22,7 @@ export default class Home extends Component {
         <PhotoList />
         <Phrase />
         {/* <Shop />
-        <Contact /> */}
+        <Contact />  */}
         <Map />
       </div>
     )
